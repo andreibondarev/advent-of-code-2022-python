13a2e3de15a84a6422f2b1f4b9951bbfe615ee88
Andrei Bondarev <andrei.bondarev13@gmail.com>
2022-12-23 13:18:53 -0600

    Day 4: Part 1

---
+++ day4/day4.py
@@ -0,0 +1,30 @@
+with open('./input.txt') as f:
+  counter = 0
+
+  for line in f:
+    sections_1, sections_2 = line.strip().split(',')
+
+    range_1_left, range_1_right = sections_1.split('-')
+    range_2_left, range_2_right = sections_2.split('-')
+
+    # We add +1 when converting range to set Python does not include end-range
+    # >>> set(range(38, 39))
+    # {38}
+    range_1_left, range_1_right = int(range_1_left), (int(range_1_right) + 1)
+    range_2_left, range_2_right = int(range_2_left), (int(range_2_right) + 1)
+
+    range_1 = range(range_1_left, range_1_right)
+    range_2 = range(range_2_left, range_2_right)
+
+    set_range_1 = set(range_1) or set({ range_1_left })
+    set_range_2 = set(range_2) or set({ range_2_left })
+
+    set_range_1_length = len(set_range_1)
+    set_range_2_length = len(set_range_2)
+    
+    intersection = set_range_1.intersection(set_range_2)
+
+    if (len(intersection) == set_range_1_length) or (len(intersection) == set_range_2_length):
+      counter += 1
+
+print(counter)
