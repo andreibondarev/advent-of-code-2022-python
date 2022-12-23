36ac2c91c5d0d33bac5e9c7dad68bdaf7f125ab6
Andrei Bondarev <andrei.bondarev13@gmail.com>
2022-12-23 13:21:56 -0600

    Day 4: complete

--- day4/day4.py
+++ day4/day4.py
@@ -16,6 +16,9 @@ with open('./input.txt') as f:
     range_1 = range(range_1_left, range_1_right)
     range_2 = range(range_2_left, range_2_right)
 
+    # >>> set(range(38,38))
+    # set()
+    # ...so we set the default to get set({ 38 })
     set_range_1 = set(range_1) or set({ range_1_left })
     set_range_2 = set(range_2) or set({ range_2_left })
 
@@ -24,7 +27,12 @@ with open('./input.txt') as f:
     
     intersection = set_range_1.intersection(set_range_2)
 
-    if (len(intersection) == set_range_1_length) or (len(intersection) == set_range_2_length):
+    # Uncomment to get Part 1 answer
+    # if (len(intersection) == set_range_1_length) or (len(intersection) == set_range_2_length):
+    #   counter += 1
+
+    # Comment out to get Part 1 answer
+    if len(intersection) > 0:
       counter += 1
 
 print(counter)
