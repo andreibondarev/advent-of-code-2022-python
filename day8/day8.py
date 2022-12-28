f01d69e58385fcd8a05ec6751a018192b9f23a0e
Andrei Bondarev <andrei.bondarev13@gmail.com>
2022-12-27 22:25:16 -0400

    Day 8: Part 2 complete

--- day8/day8.py
+++ day8/day8.py
@@ -1,6 +1,8 @@
 LINE_LENGTH = 99 - 1 # Counting position 0
+# Constant to be used for example.txt
+# LINE_LENGTH = 5 - 1 # Counting position 0
 
-def is_visible(forest, row_idx, tree_idx, count):
+def is_visible(forest, row_idx, tree_idx):
   # Trees on the edges are always visible
   if (row_idx == 0) or (tree_idx == 0) or (row_idx == LINE_LENGTH) or (tree_idx == LINE_LENGTH):
     return(True)
@@ -23,9 +25,43 @@ def is_visible(forest, row_idx, tree_idx, count):
 
     return(False)
 
+def calculate_scenic_side_score(current_tree, columns):
+  score = 0
+
+  for tree in columns:
+    if int(tree) < int(current_tree):
+      score += 1
+    elif int(tree) >= int(current_tree):
+      score += 1
+      break
+
+  return score
+
+def calculate_scenic_score(forest, row_idx, tree_idx):
+  columns_top =  [ forest[idx][tree_idx] for idx in range(row_idx) ] 
+  columns_bottom = [ forest[idx][tree_idx] for idx in range(row_idx+1, LINE_LENGTH+1) ]
+  columns_left = forest[row_idx][:tree_idx]
+  columns_right = forest[row_idx][tree_idx+1:]
+
+  current_tree = forest[row_idx][tree_idx]
+
+  # Calculate top
+  columns_top.reverse()
+  top_score = calculate_scenic_side_score(current_tree, columns_top or [])
+  # Calculate bottom
+  bottom_score = calculate_scenic_side_score(current_tree, columns_bottom or [])
+  # Calculate left
+  columns_left.reverse()
+  left_score = calculate_scenic_side_score(current_tree, columns_left or [])
+  # Calculate right
+  right_score = calculate_scenic_side_score(current_tree, columns_right or [])
+
+  return(top_score * bottom_score * left_score * right_score)
+
 with open('./input.txt') as f:
   forest = []
   visible_tree_count = 0
+  max_scenic_score = 0
 
   for line in f:
     row = line.strip()
@@ -34,7 +70,11 @@ with open('./input.txt') as f:
 
   for row_idx, row in enumerate(forest):
     for tree_idx, item in enumerate(row):
-      if is_visible(forest, row_idx, tree_idx, visible_tree_count):
+      if is_visible(forest, row_idx, tree_idx):
         visible_tree_count += 1
 
-  print(visible_tree_count)
+      scenic_score = calculate_scenic_score(forest, row_idx, tree_idx)
+      if scenic_score > max_scenic_score:
+        max_scenic_score = scenic_score
+
+  print(max_scenic_score)
